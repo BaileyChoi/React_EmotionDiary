a04ae34894a5b9dc9c6b57fabf2edaca12d89300
BingBong <bingbong9981@gmail.com>
2024-03-07 16:48:35 +0900

    Fix : 버그 수정

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+감정일기장 프로젝트
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -83,9 +83,9 @@ function App() {
 
   const [data, dispatch] = useReducer(reducer, dummyData);
 
-  console.log(new Date().getTime());
+  // console.log(new Date().getTime());
 
-  const dataId = useRef(0);
+  const dataId = useRef(6);
 
   // CREATE
   const onCreate = (date, content, emotion) => {
